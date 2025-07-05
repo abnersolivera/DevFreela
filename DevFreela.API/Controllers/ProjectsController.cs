ae8c873e4c957198a36721006d8828c312b24a19
Abner Santos <abner.santos@bdsdatasolution.com.br>
2025-07-05 16:02:06 -0300

    Refactor ProjectsController to use IProjectService consistently and handle error responses

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -8,66 +8,80 @@ namespace DevFreela.API.Controllers;
 [ApiController]
 public class ProjectsController : ControllerBase
 {
-    private readonly IProjectService _projectService;
-    public ProjectsController(IProjectService projectService)
+    private readonly IProjectService _service;
+    public ProjectsController(IProjectService service)
     {
-        _projectService = projectService;
+        _service = service;
     }
     
     [HttpGet]
     public IActionResult Get(string search = "", int page = 1, int size = 3)
     {
-        var result = _projectService.GetAll(search, page, size);
+        var result = _service.GetAll(search, page, size);
         return Ok(result);
     }
     
     [HttpGet("{id:int}")]
     public IActionResult GetById(int id)
     {
-        var result = _projectService.GetById(id);
+        var result = _service.GetById(id);
+        
+        if(!result.IsSuccess)
+            return BadRequest(result.Message);
+        
         return Ok(result);
     }
     
     [HttpPost]
     public IActionResult Post(CreateProjectInputModel inputModel)
     {
-        var result = _projectService.Insert(inputModel);
-        return CreatedAtAction(nameof(GetById), new { id = result }, inputModel);
+        var result = _service.Insert(inputModel);
+        return CreatedAtAction(nameof(GetById), new { id = result.Data }, inputModel);
     }
     
     [HttpPut("{id:int}")]
     public IActionResult Put(int id, UpdateProjectInputModel inputModel)
     {
         inputModel.IdProject = id;
-        _projectService.Update(inputModel);
+        var result = _service.Update(inputModel);
+        if (!result.IsSuccess)
+            return BadRequest(result.Message);
         return NoContent();
     }
     
     [HttpDelete("{id:int}")]
     public IActionResult Delete(int id)
     {
-        _projectService.Delete(id);
+        var result = _service.Delete(id);
+        if (!result.IsSuccess)
+            return BadRequest(result.Message);
         return NoContent();
     }
     
     [HttpPut("{id:int}/start")]
     public IActionResult Start(int id)
     {
-        _projectService.Start(id);
+        var result = _service.Start(id);
+        if (!result.IsSuccess)
+            return BadRequest(result.Message);
         return NoContent();
     }
     
     [HttpPut("{id:int}/complete")]
     public IActionResult Complete(int id)
     {
-        _projectService.Complete(id);
+        var result = _service.Complete(id);
+        if (!result.IsSuccess)
+            return BadRequest(result.Message);
         return NoContent();
     }
     
     [HttpPost("{id:int}/comments")]
     public IActionResult PostComment(int id, CreateProjectCommentInputModel inputModel)
     {
-        _projectService.InsertComment(id, inputModel);
+        var result = _service.InsertComment(id, inputModel);
+        if (!result.IsSuccess)
+            return BadRequest(result.Message);
         return NoContent();
     }
 }
