45d23346bd29d0790911bc507afb7e3ed813048e
Abner Santos <abner.santos@bdsdatasolution.com.br>
2025-07-08 18:50:20 -0300

    Remove unused using directives from GetAllProjectsHandler

--- DevFreela.Application/Queries/GetAllProjects/GetAllProjectsHandler.cs
+++ DevFreela.Application/Queries/GetAllProjects/GetAllProjectsHandler.cs
@@ -1,8 +1,6 @@
 ﻿using DevFreela.Application.Models;
 using DevFreela.Core.Repositories;
-using DevFreela.Infrastructure.Persistence;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.Application.Queries.GetAllProjects;
 
